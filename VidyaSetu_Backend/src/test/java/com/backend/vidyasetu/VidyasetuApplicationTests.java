e8bab5106ef52597844ea3e2fe6cd9c293469841
Sanika Barguje <bargujesanika02@gmail.com>
2026-08-14 12:22:04 +0530

    Change java 21 to 17

--- VidyaSetu_Backend/src/test/java/com/backend/vidyasetu/VidyasetuApplicationTests.java
+++ VidyaSetu_Backend/src/test/java/com/backend/vidyasetu/VidyasetuApplicationTests.java
@@ -1,10 +1,10 @@
-package com.vidyasetu.backend;
+package com.backend.vidyasetu;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class VidyasetuApplicationTests {
 
 	@Test
 	void contextLoads() {
